902a2a4fd663f118d74cb389b8fc1ff48ff15c57
nobodyfreeze <office.fherzog@gmail.com>
2023-07-19 12:32:54 +0200

    solution 2 / sentenceMap

--- script_01.js
+++ script_01.js
@@ -18,22 +18,41 @@ output(getSentence(["Ich","bin"],"E"));
 output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
 
 // Lösung 1
-function getSentence(arr, op) {
-    if (op === "S") {
-      // Satz erstellen, indem alle Elemente des Arrays mit Leerzeichen verbunden werden
-      return arr.join(" ");
-    } else if (op === "Q") {
-      // Satz erstellen, indem alle Elemente des Arrays mit Leerzeichen verbunden werden und ein Fragezeichen am Ende hinzugefügt wird
-      return arr.join(" ") + "?";
-    } else if (op === "E") {
-      // Satz erstellen, indem alle Elemente des Arrays ohne Leerzeichen verbunden werden
-      return arr.join("");
-    } else {
-      // Wenn die übergebene Operation ungültig ist, wird ein Fehler zurückgegeben
+// function getSentence(arr, op) {
+//     if (op === "S") {
+//       // Satz erstellen, indem alle Elemente des Arrays mit Leerzeichen verbunden werden
+//       return arr.join(" ");
+//     } else if (op === "Q") {
+//       // Satz erstellen, indem alle Elemente des Arrays mit Leerzeichen verbunden werden und ein Fragezeichen am Ende hinzugefügt wird
+//       return arr.join(" ") + "?";
+//     } else if (op === "E") {
+//       // Satz erstellen, indem alle Elemente des Arrays ohne Leerzeichen verbunden werden
+//       return arr.join("");
+//     } else {
+//       // Wenn die übergebene Operation ungültig ist, wird ein Fehler zurückgegeben
+//       return "Ungültige Operation";
+//     }
+//   }
+  
+  // Lösung 2
+  function getSentence(arr, op) {
+    // Mapping der Operationen zu den gewünschten Satzverbindungen
+    const sentenceMap = {
+      "S": " ",
+      "Q": " ",
+      "E": "",
+    };
+  
+    // Überprüfen, ob die übergebene Operation gültig ist
+    if (!Object.keys(sentenceMap).includes(op)) {
       return "Ungültige Operation";
     }
-  }
   
+    // Satz erstellen, indem alle Elemente des Arrays mit der entsprechenden Satzverbindung verbunden werden
+    const separator = sentenceMap[op];
+    return arr.join(separator) + (op === "Q" ? "?" : "");
+  }
+
   // mögliche Tests:
   console.log(getSentence(["Ich","bin","Max"], "S")); // Ausgabe: "Ich bin Max"
   console.log(getSentence(["Bist","du","Max"], "Q")); // Ausgabe: "Bist du Max?"
